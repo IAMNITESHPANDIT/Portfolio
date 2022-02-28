164af025d23494c84830afbea299c226f166d579
nitesh <niteshpandit676@gmail.com>
2022-02-28 06:14:16 +0530

    about section completed

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -5,17 +5,20 @@ function About() {
   return (
     <div className="container">
         <div className="row">
-            <div className="col-12 col-md-6 text-md-center">
-            <div className="boxOne ms-5 mt-5 ">
+            <div className="col-12 col-md-4 ">
+            <div className="boxOne mt-5 ">
             <img className="proImg" src={Profile} alt="profile" className='img-fluid' />
             </div>
             </div>
-            <div className="col-12 col-md-6 boxTwo">
-                
+            <div className="col-12 col-md-8 boxTwo mt-5 text-start ">
+                <p className="h2">ABOUT ME</p>
+                <p className="p lead">Lorem ipsum Lorem Lorem ipsum 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint debitis minus error velit natus earum.
+                dolor sit amet consectetur, adipisicing elit. ipsum dolor sit amet consectetur adipisicing elit. Eligendi exercitationem alias, tempora qui numquam reprehenderit. Sunt totam nihil reprehenderit placeat? dolor sit amet consectetur adipisicing elit. Eius, dignissimos.
+                </p>
+                <button className="readmeBtn rounded">Read More</button>
             </div>
         </div>
-
-
     </div>
   )
 }
