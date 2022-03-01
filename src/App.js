6cc31d4e097592545405f4893c791cbfc4328294
nitesh <niteshpandit676@gmail.com>
2022-03-01 20:55:47 +0530

    footer component added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from  './components/about/about.component';
 import Service from  './components/service/service.component';
 import Portfolio from './components/portfolio/portfolio.component';
 import Review from './components/review/review.component';
+import Footer from './components/footer/footer.component';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
     <Service/>
     <Portfolio/>
     <Review/>
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div class="container text-center bg-secondary mt-5 p-2 text-white">
+        <p className="h3">@Copyright Iamniteshpandit</p>
+    </div>
+  )
+}
+
+export default Footer
