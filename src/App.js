2ef3a50fbd74b9165c7c510859fd2064e24ca4a6
nitesh <niteshpandit676@gmail.com>
2022-03-01 12:14:06 +0530

    review component prepared

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import NavBar from './components/Navbar/navbar';
 import About from  './components/about/about.component';
 import Service from  './components/service/service.component';
+import Portfolio from './components/portfolio/portfolio.component';
+import Review from './components/review/review.component';
 function App() {
   return (
     <div className="App">
     <NavBar/>
     <About/>
     <Service/>
+    <Portfolio/>
+    <Review/>
     </div>
   );
 }
---
+++ src/components/review/review.component.jsx
@@ -0,0 +1,39 @@
+import './review.style.css';
+import accountIcon from '../../assest/icon/ac.png';
+import React from 'react'
+
+function Review() {
+  return (
+    <div className="container p-4 m-4 ">
+        <div className="row expert">
+            <div className="col-4">
+                <p className=" h1 text-center pt-5 mt-5">What Export says About us</p>
+            </div>
+            <div className="col-4">
+                <div className="first m-3 text-black p-4">
+                    <p className="h2">"Best Developer"</p>
+                    <p className="lead ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, doloremque?</p>
+                    <img src={accountIcon} class="img-fluid" alt="ac icon"/>
+                    <span className="ps-2">John Franklin</span>
+                    <br/>
+                    <span className="ps-4">Chennai ,Tamilnadu, India</span>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="first m-3 text-black p-4">
+                    <p className="h2">"Creative Dev"</p>
+                    <p className="lead ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, doloremque?</p>
+                    <img src={accountIcon} class="img-fluid" alt="ac icon"/>
+                    <span className="ps-2">Nitesh Pandit</span>
+                    <br/>
+                    <span className="ps-4">Gorkhpur ,U.P, India</span>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Review;
