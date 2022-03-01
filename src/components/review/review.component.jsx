6387be033faa07938513ef5efc85c2f6d3d8e537
nitesh <niteshpandit676@gmail.com>
2022-03-01 20:15:49 +0530

    review component intigrated and also fixed some mobile responsiveness

--- src/components/review/review.component.jsx
+++ src/components/review/review.component.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 function Review() {
   return (
-    <div className="container p-4 m-4 ">
+    <div className="container ps-4 m-4 ">
         <div className="row expert">
-            <div className="col-4">
+            <div className="col-12 col-md-4">
                 <p className=" h1 text-center pt-5 mt-5">What Export says About us</p>
             </div>
-            <div className="col-4">
-                <div className="first m-3 text-black p-4">
+            <div className="col-12 col-md-4">
+                <div className="first  m-3 text-black p-4">
                     <p className="h2">"Best Developer"</p>
                     <p className="lead ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, doloremque?</p>
                     <img src={accountIcon} class="img-fluid" alt="ac icon"/>
@@ -19,7 +19,7 @@ function Review() {
                     <span className="ps-4">Chennai ,Tamilnadu, India</span>
                 </div>
             </div>
-            <div className="col-4">
+            <div className="col-12 col-md-4">
                 <div className="first m-3 text-black p-4">
                     <p className="h2">"Creative Dev"</p>
                     <p className="lead ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, doloremque?</p>
